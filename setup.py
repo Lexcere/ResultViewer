1e7d7f9c060aed619aa311f6cbcbdd51f2119c4b
Lexcere <73228491+Lexcere@users.noreply.github.com>
2022-08-11 16:04:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+description = "Result viewer"
+
+with open("requirements.txt") as fid:
+    install_requires = fid.readlines()
+
+setuptools.setup(
+    name="result_viewer",
+    version="1.0",
+    author="Cere",
+    author_email="cere@cere.com",
+    description=description,
+    long_description=description,
+    python_requires=">=3.6.0",
+    url="https://github.com/Lexcere/ResultViewer",
+    install_requires=install_requires,
+    package_dir={"result_viewer": "result_viewer"},
+    packages=["result_viewer"],
+    entry_points={
+        "console_scripts": ["result-viewer = result_viewer.main:main"]
+    },
+    include_package_data=True,
+)
