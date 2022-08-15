b9895e6b64f160e82756963bc7972c926e0aecbc
alex cereghetti <lex.cere@gmail.com>
2022-08-15 19:18:54 +0200

    add optional positional argument

--- src/main.py
+++ src/main.py
@@ -2,17 +2,21 @@ import argparse
 import ResultsParser
 import ui
 import sys
+import os
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('dir', nargs='?', default=os.getcwd())
     parser.add_argument("-r", "--recursive", help="select to search recursively inside folder or not", action="store_true")
     args = parser.parse_args()
+
     if len(sys.argv) == 1:
         ui.main()
 
-    if args.recursive:
-        print("recu")
+    else:
+        parser = ResultsParser.TestResultParser(folder_path=args.dir, recursive=args.recursive)
+        print(parser.metrics())
 
 
 if __name__ == "__main__":
