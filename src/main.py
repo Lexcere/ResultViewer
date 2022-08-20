78065c2a8116be59ae1a1a6ac5a8b86d712380b3
alex cereghetti <lex.cere@gmail.com>
2022-08-20 20:42:15 +0200

    remove "metric" option
    this will be default value when no other option are given

--- src/main.py
+++ src/main.py
@@ -21,7 +21,6 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', nargs='?', default=os.getcwd())
     parser.add_argument("-r", "--recursive", help="select to search recursively inside folder or not", action="store_true")
-    parser.add_argument("-m", "--metrics", help="print metrics", action="store_true")
     parser.add_argument("-c", "--count", help="print metrics", action="store_true")
     parser.add_argument("-k", "--checksum", help="return 0 if checksum is valid", action="store_true")
     args = parser.parse_args()
@@ -31,16 +30,16 @@ def main():
 
     elif args.dir:
         parser = ResultsParser.TestResultParser(folder_path=args.dir, recursive=args.recursive)
-        if args.metrics:
+        if args.count:
+            print(parser.count())
+        elif args.checksum:
+            print(parser.is_checksum_valid())
+        else:
             print(f"{Bcolors.BOLD}{Bcolors.UNDERLINE}Total: {parser.metrics()['total']}{Bcolors.ENDC}")
             print(f"{Bcolors.OKGREEN}Pass: {parser.metrics()['pass']}{Bcolors.ENDC}")
             print(f"{Bcolors.FAIL}Fail: {parser.metrics()['fail']}{Bcolors.ENDC}")
             print(f"{Bcolors.WARNING}Skip: {parser.metrics()['skip']}{Bcolors.ENDC}")
             print(f"{Bcolors.OKCYAN}Other: {parser.metrics()['other']}{Bcolors.ENDC}")
-        elif args.count:
-            print(parser.count())
-        elif args.checksum:
-            print(parser.is_checksum_valid())
 
 
 if __name__ == "__main__":
