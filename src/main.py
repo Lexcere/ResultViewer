c4b751e1a2887afd274bac3766cda1f30fe818c1
alex cereghetti <lex.cere@gmail.com>
2022-09-28 07:23:08 +0200

    add report argument and use show

--- src/main.py
+++ src/main.py
@@ -4,22 +4,31 @@ import ui
 import sys
 import os
 import colorama
+from Plugins import ReportHTML
 
 colorama.init()  # this is necessary for window coloring
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('dir', nargs='?', default=os.getcwd())
-    parser.add_argument("-r", "--recursive", help="select to search recursively inside folder or not", action="store_true")
-    parser.add_argument("-c", "--count", help="print metrics", action="store_true")
-    parser.add_argument("-k", "--checksum", help="return 0 if checksum is valid", action="store_true")
+    subparsers = parser.add_subparsers(dest="command", required=False)
+    parser_show = subparsers.add_parser("show", help="show help", aliases=['sh'])
+    parser_report = subparsers.add_parser("report", help="report help", aliases=['rp'])
+
+    parser_show.add_argument('dir', nargs='?', default=os.getcwd())
+    parser_show.add_argument("-r", "--recursive", help="select to search recursively inside folder or not", action="store_true")
+    parser_show.add_argument("-c", "--count", help="print metrics", action="store_true")
+    parser_show.add_argument("-k", "--checksum", help="return 0 if checksum is valid", action="store_true")
+
+    parser_report.add_argument('dir', nargs='?', default=os.getcwd())
+    # parser_report.add_argument('-o', "--output", help="select where to save report", action="store_true")
+
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
         ui.main()
 
-    elif args.dir:
+    elif args.command == "show" or args.command == "sh":
         parser = ResultsParser.TestResultParser(folder_path=args.dir, recursive=args.recursive)
         if args.count:
             print(parser.count())
@@ -31,6 +40,11 @@ def main():
             print(f"{colorama.Fore.RED}Fail: {parser.metrics()['fail']}")
             print(f"{colorama.Fore.YELLOW}Skip: {parser.metrics()['skip']}")
             print(f"{colorama.Fore.BLUE}Other: {parser.metrics()['other']}")
+
+    elif args.command == "report" or args.command == "rp":
+        parser = ResultsParser.TestResultParser(folder_path=args.dir, recursive=False)
+        ReportHTML.ReportHTML(args.dir, parser.get_files())
+        print(f"Report generated in {args.dir}")
 
 
 if __name__ == "__main__":
