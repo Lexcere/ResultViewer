9ee07de119331c3a611b1018642d1a7d87f87fde
Alex Cereghetti <alex.cereghetti@comet.ch>
2022-09-28 16:13:20 +0200

    add toolbar instead

--- src/ui.py
+++ src/ui.py
@@ -1,8 +1,8 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
-from PyQt5.QtWidgets import QMenuBar, QMenu
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QSpinBox, QCheckBox
+from PyQt5.QtWidgets import QMenuBar, QMenu, QToolBar
 
 
 class Window(QMainWindow):
@@ -19,7 +19,8 @@ class Window(QMainWindow):
         self.centralWidget = QLabel("Hello, World")
         self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.setCentralWidget(self.centralWidget)
-        self._create_menu_bar()
+        # self._create_menu_bar()
+        self._create_tool_bars()
 
     def _create_menu_bar(self):
         menuBar = self.menuBar()
@@ -31,6 +32,23 @@ class Window(QMainWindow):
         helpMenu = menuBar.addMenu("&Help")
 
         self.setMenuBar(menuBar)
+
+    def _create_tool_bars(self):
+        main_tool_bar = QToolBar("Toolbar", self)
+        self.addToolBar(Qt.TopToolBarArea, main_tool_bar)
+
+        # add checkbox
+        self.recursive_label = QLabel("Recursive")
+        main_tool_bar.addWidget(self.recursive_label)
+
+        self.recursive_check_box = QCheckBox()
+        self.recursive_check_box.setFocusPolicy(Qt.NoFocus)
+        main_tool_bar.addWidget(self.recursive_check_box)
+
+        # add spin box
+        self.fontSizeSpinBox = QSpinBox()
+        self.fontSizeSpinBox.setFocusPolicy(Qt.NoFocus)
+        main_tool_bar.addWidget(self.fontSizeSpinBox)
 
 
 def main():
