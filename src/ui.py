63eec3aea21f541efdae0824a768b558ad5b9950
Alex Cereghetti <alex.cereghetti@comet.ch>
2022-09-29 15:55:57 +0200

    add other element to toolbar

--- src/ui.py
+++ src/ui.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QSpinBox, QCheckBox
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QSpinBox, QCheckBox, QPushButton, QTreeWidget, QGridLayout
 from PyQt5.QtWidgets import QMenuBar, QMenu, QToolBar
 
 
@@ -37,6 +37,14 @@ class Window(QMainWindow):
         main_tool_bar = QToolBar("Toolbar", self)
         self.addToolBar(Qt.TopToolBarArea, main_tool_bar)
 
+        # add open button
+        self.open_button = QPushButton("Open")
+        main_tool_bar.addWidget(self.open_button)
+
+        # add refresh button
+        self.refresh_button = QPushButton("Refresh")
+        main_tool_bar.addWidget(self.refresh_button)
+
         # add checkbox
         self.recursive_label = QLabel("Recursive")
         main_tool_bar.addWidget(self.recursive_label)
@@ -49,6 +57,15 @@ class Window(QMainWindow):
         self.fontSizeSpinBox = QSpinBox()
         self.fontSizeSpinBox.setFocusPolicy(Qt.NoFocus)
         main_tool_bar.addWidget(self.fontSizeSpinBox)
+
+        # add choose path
+        self.path_label = QLabel("my/choosen/path")
+        main_tool_bar.addWidget(self.path_label)
+
+        # add generate button
+        self.generate_button = QPushButton("Generate report")
+        self.generate_button.setToolTip("Generate html report from current directory")
+        main_tool_bar.addWidget(self.generate_button)
 
 
 def main():
