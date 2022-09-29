b43f6c8217d1f477eeb6024cf5664ce809d701ba
Alex Cereghetti <alex.cereghetti@comet.ch>
2022-09-29 12:41:56 +0200

    needed but no idea why

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
-from . import ResultsParser, ui
+# For relative imports to work in Python 3.6
+# https://itsmycode.com/importerror-attempted-relative-import-with-no-known-parent-package/
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
