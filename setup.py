e473ff5559a8467d2f9602d38ca4c318a53559a1
Alex Cereghetti <alex.cereghetti@comet.ch>
2022-10-03 14:32:01 +0200

    remove info from setup.py
    since info are now contained in pyproject.toml

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-import setuptools
+from setuptools import setup
 
-description = "Result viewer"
-
-with open("requirements.txt") as fid:
-    install_requires = fid.readlines()
-
-setuptools.setup(
-    name="result_viewer",
-    version="0.0.1",
-    author="Cere",
-    author_email="cere@cere.com",
-    description=description,
-    long_description=description,
-    python_requires=">=3.6.0",
-    url="https://github.com/Lexcere/ResultViewer",
-    install_requires=install_requires,
-    package_dir={"result_viewer": "src"},
-    packages=["result_viewer", "result_viewer.Plugins"],
-    entry_points={
-        "console_scripts": ["results = result_viewer.main:main"]
-    },
-    include_package_data=True,
-)
+setup()
